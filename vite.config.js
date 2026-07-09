e3c6baf018e311902c65e671e58603351dbb01d0
durisetivarshini-ux <durisetivarshini@gmail.com>
2026-07-09 18:54:29 +0530

    fix: change Vite base path to relative './' to fix blank page rendering

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/business-brain/',
+  base: './',
   plugins: [react()],
   resolve: {
     alias: {
